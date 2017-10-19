c0815cc8aa060ac300e53df3ea9d0fda389e95e0
Nico Villanueva <villanueva.arg.@gmail.com>
2017-10-19 16:13:35 -0300

    verbose option

--- npot.py
+++ npot.py
@@ -5,14 +5,20 @@ import glob
 args = argparse.ArgumentParser()
 args.add_argument('--path', '-p', type=str, default='.', help='Where to look (default: \'.\')')
 args.add_argument('--extension', '-e', type=str, default='png', help='Extension to look for (default: png)')
+args.add_argument('--verbose', '-v', action='store_true', help='Print a lot of shit (default: disabled, only print non-POT images)')
 args = args.parse_args()
+
+
+def debug(message):
+    if args.verbose:
+        print(message)
 
 
 def main():
     target = '{}/**/*.{}'.format(args.path, args.extension)
-    print('Globbing: {}'.format(target))
+    debug('Globbing: {}'.format(target))
     for filename in glob.glob(target, recursive=True):
-        print('Checking out: {}'.format(filename))
+        debug('Checking out: {}'.format(filename))
         try:
             img = Image.open(filename)
         except OSError:
@@ -25,7 +31,7 @@ def main():
         if not (((wid & (wid - 1)) == 0) and wid != 0):
             print('{} dimensions are not a power of 2 (wid/hei: {})'.format(filename, wid))
             continue
-        print('{} is alright'.format(filename))
+        debug('{} is alright'.format(filename))
 
 
 if __name__ == '__main__':
